5013fa2d1e6929875cc1522eb83c9972cbcc5235
higo <higoedu@gmail.com>
2023-02-15 21:25:42 -0300

    criou classe hellow world
    criou classe hellow world

---
+++ aula05/HellowWorld.java
@@ -0,0 +1,21 @@
+package aula05;
+/*
+nome da classe é o memo no do arquivo.java
+HellowWorld é camel case, pois tem mais de uma palavra junta
+
+- declaração da classe - class
+- nome da classe - HellowWorld
+- nome do método - main
+- modificadores de acesso do método - public static
+- tipo de retorno do método - void (método não retorna nada)
+- parâmetros ou argumentos do método - (String[] args)
+- comando que faz output na tela - System.out.println
+- blocos, cada parte do programa {}, são delimitados entre chaves
+
+Java é case sensitive, por exemplo se p de public for maiúsculo, não vai funcionar
+*/
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hellow World!");
+    }
+}
