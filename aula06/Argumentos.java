2addf43ca8b9a928f0b6c0230ad03d1508d4b210
higo <higoedu@gmail.com>
2023-07-16 21:44:03 -0300

    adicionou comentário
    adicionou comentário

--- aula06/Argumentos.java
+++ aula06/Argumentos.java
@@ -1,7 +1,19 @@
 package aula06;
 
 public class Argumentos {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//método main recebe argumentos do tipo array de String
         System.out.println("Você digitou " + args[0]);
     }
 }
+/*
+javac Argumentos.java
+java Argumentos Higo
+
+vai imprimir
+Você digitou Higo
+
+java Argumentos "Higo Souza"
+
+vai imprimir
+Você digitou Higo Souza
+*/
